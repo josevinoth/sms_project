4ba2719050b741aa92f2de8d0f7283c0b44996bf
Jose <josevinoth83@gmail.com>
2022-09-02 00:20:17 +0530

    latest commit -Jose( 01-Sep-2022)

--- SMS/sms_app/sub_views/locationmaster_add_view.py
+++ SMS/sms_app/sub_views/locationmaster_add_view.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
 from ..forms import LocationmasteraddForm
-from ..models import LocationmasterInfo,CustomerInfo,TrbusinesstypeInfo
+from ..models import LocationmasterInfo,CustomerInfo,TrbusinesstypeInfo,Warehouse_goods_info
 from django.shortcuts import render, redirect
 
 @login_required(login_url='login_page')
@@ -68,6 +68,32 @@ def locationmaster_add(request,locationmaster_id=0):
                 locationmaster = LocationmasterInfo.objects.get(pk=locationmaster_id)
                 form = LocationmasteraddForm(request.POST, instance=locationmaster)
                 if form.is_valid():
+                    Branch_val = LocationmasterInfo.objects.get(pk=locationmaster_id).lm_wh_location.id
+                    Unit_val = LocationmasterInfo.objects.get(pk=locationmaster_id).lm_wh_unit.id
+                    Bay_val = LocationmasterInfo.objects.get(pk=locationmaster_id).lm_areaside.id
+                    print(Branch_val)
+                    print(Unit_val)
+                    print(Bay_val)
+                    wh_goods_list = Warehouse_goods_info.objects.filter(wh_branch_id=Branch_val, wh_unit_id=Unit_val,wh_bay_id=Bay_val)
+                    stack_layer = wh_goods_list.values('wh_stack_layer_id')
+                    volume = wh_goods_list.values('wh_goods_volume_weight')
+                    area = wh_goods_list.values('wh_goods_area')
+                    area_final = 0
+                    volume_final = 0
+                    for j in range(len(stack_layer)):
+                        # print("Stack Layer",stack_layer[j]['wh_stack_layer_id'])
+                        # print("Volume Occupied", volume[j]['wh_goods_volume_weight'])
+                        volume_final = volume_final + volume[j]['wh_goods_volume_weight']
+                        if stack_layer[j]['wh_stack_layer_id'] == 1:
+                            # print("Area Occupied", area[j]['wh_goods_area'])
+                            area_final = area_final + area[j]['wh_goods_area']
+                        else:
+                            print("No Area")
+                    print("Total_Area", area_final)
+                    print("Total_Volume",volume_final)
+                    LocationmasterInfo.objects.filter(pk=locationmaster_id).update(lm_area_occupied=area_final)
+                    LocationmasterInfo.objects.filter(pk=locationmaster_id).update(lm_volume_occupied=volume_final)
+
                     form.save()
             return redirect('/SMS/locationmaster_list')
 
@@ -84,6 +110,12 @@ def locationmaster_delete(request,locationmaster_id):
     locationmaster = LocationmasterInfo.objects.get(pk=locationmaster_id)
     locationmaster.delete()
     return redirect('/SMS/locationmaster_list')
+
+#Update Location master
+@login_required(login_url='login_page')
+def update_location_master(request):
+    wh_branch=LocationmasterInfo.object.all()
+    print(wh_branch)
 
 #Get Customer Model
 @login_required(login_url='login_page')
