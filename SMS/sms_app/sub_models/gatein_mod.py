39ffb4de2591cb017f77db818d69a56ab048fab9
BVM <josevinoth83@gmail.com>
2023-02-15 22:01:05 +0530

    Model files Update_Commit_15-Feb-23-Jose

--- SMS/sms_app/sub_models/gatein_mod.py
+++ SMS/sms_app/sub_models/gatein_mod.py
@@ -26,7 +26,7 @@ class Gatein_info(models.Model):
     gatein_updated_at = models.DateTimeField(null=True,auto_now=True)
     gatein_updated_by = models.ForeignKey(MyUser,on_delete=models.CASCADE,null=True)
     gatein_job_status = models.ForeignKey(GstexcemptionInfo, on_delete=models.CASCADE, default=2)
-    gatein_po_num = models.CharField(blank=True, null=True, max_length=20)
+    gatein_po_num = models.CharField(blank=True, null=True, max_length=100)
 
     class Meta:
         ordering = ["gatein_job_no"]
