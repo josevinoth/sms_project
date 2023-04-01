9f757a08ff5cb3e45e19aba58fc09645fd1e2413
josevinoth <101917197+josevinoth@users.noreply.github.com>
2023-04-01 08:59:29 +0530

    latest Update_Commit_1-Apr-23-Jose

--- SMS/sms_app/sub_models/sales_info_mod.py
+++ SMS/sms_app/sub_models/sales_info_mod.py
@@ -41,7 +41,7 @@ class SalesInfo(models.Model):
     s_contract = models.ForeignKey(YesNoInfo,blank=True, null=True, on_delete=models.CASCADE,related_name='s_contract', db_column='s_contract', default='')
     s_rate_approval = models.ForeignKey(YesNoInfo,blank=True, null=True, related_name='s_rate_approval', db_column='s_rate_approval',on_delete=models.CASCADE, default='')
     s_expected_prof = models.IntegerField(blank=True, null=True)
-    s_approver_name = models.CharField(blank=True, null=True,max_length=30)
+    s_approver_name = models.ForeignKey(MyUser, related_name='s_approver_name', db_column='s_approver_name',on_delete=models.CASCADE, blank=True, null=True,)
     s_status = models.ForeignKey(Salestatus,blank=True, null=True, on_delete=models.CASCADE, default='')
     s_remark = models.TextField(blank=True, null=True)
     s_next_meet_schd_date = models.DateField(null = True,blank=True)
