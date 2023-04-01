c4ac518a703eb221da057b9d7bcd72f40c6b657d
josevinoth <101917197+josevinoth@users.noreply.github.com>
2023-04-01 09:03:50 +0530

    latest Update_Commit_01-Apr-23-Jose

--- SMS/sms_app/sub_models/my_user_mod.py
+++ SMS/sms_app/sub_models/my_user_mod.py
@@ -2,6 +2,6 @@ from django.contrib.auth.models import User
 class MyUser(User):
     class Meta:
         proxy = True
-
+        ordering = ["first_name"]
     def __str__(self):
         return self.get_full_name()
