f13f0e3b08456e4a8cdd3b2d1586ab32ac929cfd
josevinoth <101917197+josevinoth@users.noreply.github.com>
2023-09-16 15:30:09 +0530

    Latest Update-Jose (16-Sep-2023)

--- SMS/sms_app/sub_views/gatein_pre_add_view.py
+++ SMS/sms_app/sub_views/gatein_pre_add_view.py
@@ -74,7 +74,15 @@ def gatein_pre_add(request, gatein_pre_id=0):
 @login_required(login_url='login_page')
 def gatein_pre_list(request):
     first_name = request.session.get('first_name')
-    context = {'Gatein_pre_list' : Gatein_pre_info.objects.all(),'first_name': first_name,}
+    Gatein_pre_list= Gatein_pre_info.objects.all()
+    page_number = request.GET.get('page')
+    paginator = Paginator(Gatein_pre_list, 100000000)
+    page_obj = paginator.get_page(page_number)
+    context = {
+        # 'Gatein_pre_list' : Gatein_pre_info.objects.all(),
+        'first_name': first_name,
+        'page_obj': page_obj,
+    }
     return render(request,"asset_mgt_app/gatein_pre_list.html",context)
 
 #Delete WH Job
