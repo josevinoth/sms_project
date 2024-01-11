e8224994604a3a679a0738296cf068a24efb1590
josevinoth <101917197+josevinoth@users.noreply.github.com>
2024-01-11 12:50:40 +0530

    Latest Update-Jose (11-Jan-2024)

--- SMS/sms_app/sub_forms/pregatein_truckform_Form.py
+++ SMS/sms_app/sub_forms/pregatein_truckform_Form.py
@@ -13,6 +13,4 @@ class PregateintruckForm(forms.ModelForm):
         self.fields['pregatein_updated_by'].empty_label = "--Select--"
         self.fields['pregatein_otl_check'].empty_label = "--Select--"
         self.fields['pregatein_offload_acceptance'].empty_label = "--Select--"
-        self.fields['pregatein_pouch'].empty_label = "--Select--"
-        self.fields['pregatein_pouch_yes'].empty_label = "--Select--"
         self.fields['pregatein_otl_type'].empty_label = "--Select--"
