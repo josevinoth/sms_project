576a230644a39a355e44bfcfde613974c6b1acc1
josevinoth <101917197+josevinoth@users.noreply.github.com>
2024-02-13 21:08:49 +0530

    Commit-Jose(13-Feb-2024)

--- SMS/sms_app/sub_models/asset_info_mod.py
+++ SMS/sms_app/sub_models/asset_info_mod.py
@@ -2,16 +2,16 @@ from django.db import models
 from ..models import Product_info,Department_info,MyUser,Location_info,Vendor_info,Insurance_Info,UnitInfo
 
 class AssetInfo(models.Model):
-    asset_number = models.CharField(max_length=10,default = '')
+    asset_number = models.CharField(max_length=50,null=True,blank=True)
     asset_product = models.ForeignKey(Product_info, on_delete=models.CASCADE, default='')
     asset_unit = models.ForeignKey(UnitInfo, on_delete=models.CASCADE, default='')
-    asset_model = models.CharField(max_length=30)
-    asset_make = models.CharField(max_length=30)
+    asset_model = models.CharField(max_length=50)
+    asset_make = models.CharField(max_length=50)
     asset_assignedto = models.ForeignKey(MyUser,on_delete=models.CASCADE, related_name='asset_assignedto',db_column='asset_assignedto',default='')
-    asset_serial_num1 = models.CharField(max_length=10,default='')
-    asset_assignedon = models.CharField(blank=True, null=True,max_length=30)
-    asset_cost = models.DecimalField(max_digits=10, decimal_places=2,default=0.0)
-    asset_order_number=models.CharField(blank=True, null=True,max_length=30)
+    asset_serial_num1 = models.CharField(max_length=50,default='')
+    asset_assignedon = models.CharField(blank=True, null=True,max_length=50)
+    asset_cost = models.DecimalField(max_digits=50, decimal_places=2,default=0.0)
+    asset_order_number=models.CharField(blank=True, null=True,max_length=50)
     asset_purchase_date=models.DateField()
     asset_location = models.ForeignKey(Location_info, on_delete=models.CASCADE, default='')
     asset_vendor = models.ForeignKey(Vendor_info, on_delete=models.CASCADE, default='')
