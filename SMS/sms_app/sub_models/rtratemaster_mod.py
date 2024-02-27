9b433ff179443624c86d3122e32236b6721afad9
josevinoth <101917197+josevinoth@users.noreply.github.com>
2024-02-27 23:11:44 +0530

    Commit-Jose (27-Feb-24)

--- SMS/sms_app/sub_models/rtratemaster_mod.py
+++ SMS/sms_app/sub_models/rtratemaster_mod.py
@@ -1,5 +1,5 @@
 from django.db import models
-from ..models import City,CustomerInfo,CustomerdepartmentInfo,VehicletypeInfo,VehiclecategoryInfo,Places
+from ..models import MyUser,CustomerInfo,CustomerdepartmentInfo,VehicletypeInfo,VehiclecategoryInfo,Places
 
 class RtratemasterInfo(models.Model):
     ro_fromlocation = models.ForeignKey(Places, on_delete=models.CASCADE, default='',related_name='ro_fromlocation', db_column='ro_fromlocation')
@@ -13,8 +13,8 @@ class RtratemasterInfo(models.Model):
     ro_touchpoint2 = models.ForeignKey(Places, on_delete=models.CASCADE, related_name='ro_touchpoint2',db_column='ro_touchpoint2', null=True, blank=True)
     ro_touchpoint3 = models.ForeignKey(Places, on_delete=models.CASCADE, related_name='ro_touchpoint3',db_column='ro_touchpoint3', null=True, blank=True)
     ro_touchpoint4 = models.ForeignKey(Places, on_delete=models.CASCADE, related_name='ro_touchpoint4',db_column='ro_touchpoint4', null=True, blank=True)
-
-
-
+    ro_created_at = models.DateTimeField(null=True, auto_now_add=True)
+    ro_updated_at = models.DateTimeField(null=True, auto_now=True)
+    ro_updated_by = models.ForeignKey(MyUser, on_delete=models.CASCADE, null=True)
     def __str__(self):
         return self.ro_rate
--- SMS/sms_app/sub_views/rtratemaster_add_view.py
+++ SMS/sms_app/sub_views/rtratemaster_add_view.py
@@ -7,13 +7,14 @@ from django.shortcuts import render, redirect
 @login_required(login_url='login_page')
 def rtratemaster_add(request,rtratemaster_id=0):
     first_name = request.session.get('first_name')
+    user_id = request.session.get('ses_userID')
     if request.method == "GET":
         if rtratemaster_id == 0:
             form = RtratemasteraddForm()
         else:
             rtratemaster = RtratemasterInfo.objects.get(pk=rtratemaster_id)
             form = RtratemasteraddForm(instance=rtratemaster)
-        return render(request, "asset_mgt_app/rtratemaster_add.html", {'form': form,'first_name': first_name})
+        return render(request, "asset_mgt_app/rtratemaster_add.html", {'form': form,'first_name': first_name,'user_id':user_id,})
     else:
         if rtratemaster_id == 0:
             form = RtratemasteraddForm(request.POST)
