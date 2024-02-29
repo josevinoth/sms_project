4534bdbbfec5700cab0fcb1c5efb78394ac81c22
josevinoth <101917197+josevinoth@users.noreply.github.com>
2024-02-29 14:27:21 +0530

    Commit-Jose (29-Feb-24)

--- SMS/sms_app/sub_forms/tripclosureform_Form.py
+++ SMS/sms_app/sub_forms/tripclosureform_Form.py
@@ -5,7 +5,6 @@ class TripclosureaddForm(forms.ModelForm):
 
     class Meta:
         model = TripdetailInfo
-        # fields = '__all__'
         fields = ['tr_updated_by','tr_enquirynumber','tr_consignmentnumber','tr_tripnumber','tr_vehicletype','tr_vehiclesource','tr_vehiclenumber','tr_vehicletype_placed','tr_category','tr_departedlocation','tr_reportedlocation','tc_tripcost','tc_parkingcost','tc_tollcost','tc_loadingcost','tc_unloadingcost','tc_weighmentcost','tc_handlingcost','tr_iou','tc_financestatus']
     def __init__(self, *args, **kwargs):
         super(TripclosureaddForm,self).__init__(*args, **kwargs)
