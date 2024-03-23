9c17bb423d9fa761777e626db2dce7b30bffab7b
josevinoth <101917197+josevinoth@users.noreply.github.com>
2024-03-23 08:56:05 +0530

    Commit-Jose (23-Mar-24)

--- SMS/sms_app/sub_models/na_dimension_mod.py
+++ SMS/sms_app/sub_models/na_dimension_mod.py
@@ -11,10 +11,10 @@ class Nadimension(models.Model):
     nad_updated_at = models.DateTimeField(null=True, auto_now=True)
     nad_updated_by = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='nad_updated_by',db_column='nad_updated_by')
     nad_wood_type = models.ForeignKey(Pkstocktype, on_delete=models.CASCADE,limit_choices_to={'id__in': [1, 4]})
-    nad_wood_description = models.ForeignKey(Stockdescription, on_delete=models.CASCADE, default='')
+    nad_wood_description = models.ForeignKey(Stockdescription, on_delete=models.CASCADE, default='',blank=True,null=True)
     nad_job_weight = models.FloatField( default=0.0)
     nad_type_of_req = models.ForeignKey(Natypeofreq, on_delete=models.CASCADE)
-    nad_uom = models.ForeignKey(Unitofmeasure, on_delete=models.CASCADE, default='', limit_choices_to={'id__in': [1,2,3,4]})
+    nad_uom = models.ForeignKey(Unitofmeasure, on_delete=models.CASCADE, default='', limit_choices_to={'id__in': [1,2,3,4]},blank=True,null=True)
     nad_plywood_thickness = models.FloatField(default=0.0)
     class Meta:
         ordering = ["nad_assess_num"]
