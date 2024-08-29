83217e8636a968e9fb5f5b58a1762a06d66f04d8
josevinoth <101917197+josevinoth@users.noreply.github.com>
2024-08-29 10:49:07 +0530

    Commit-Jose (29-Aug-24)

--- SMS/sms_app/sub_models/task_mod.py
+++ SMS/sms_app/sub_models/task_mod.py
@@ -3,8 +3,8 @@ from ..models import RequirementsInfo,MyUser,applicaiton_Info
 
 class task_Info(models.Model):
     application = models.ForeignKey(applicaiton_Info,on_delete=models.CASCADE, related_name='application',db_column='application',default=1)
-    main_task = models.CharField(max_length=100)
-    sub_task = models.CharField(max_length=100)
+    main_task = models.TextField(max_length=100)
+    sub_task = models.TextField(max_length=100)
     remarks = models.TextField(max_length=30, blank=True, null=True)
     task_id = models.CharField(max_length=100,blank=True,null=True)
     t_updated_at = models.DateTimeField(null=True, auto_now=True)
