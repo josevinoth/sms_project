ebedfb5df021829f25f18160f6c287b72eede9b3
josevinoth <101917197+josevinoth@users.noreply.github.com>
2024-09-12 10:31:01 +0530

    Commit-Jose (12-Sep-24) Timesheet comments field character increased from 30 to 500

--- SMS/sms_app/sub_models/timesheet_mod.py
+++ SMS/sms_app/sub_models/timesheet_mod.py
@@ -7,7 +7,7 @@ class timesheet_Info(models.Model):
     ts_end_date = models.DateTimeField()
     ts_hours = models.FloatField(default=0.0)
     ts_developer = models.ForeignKey(MyUser, on_delete=models.CASCADE,related_name='ts_developer', db_column='ts_developer')
-    ts_remarks = models.TextField(max_length=30, blank=True, null=True)
+    ts_remarks = models.TextField(max_length=500, blank=True, null=True)
     ts_updated_at = models.DateTimeField(null=True, auto_now=True)
     ts_updated_by = models.ForeignKey(MyUser, related_name='ts_updated_by', db_column='ts_updated_by',
                                      on_delete=models.CASCADE, null=True)
