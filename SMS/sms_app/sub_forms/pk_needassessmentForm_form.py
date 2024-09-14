b89cbc46bb20aa9a33b5b959c9d7a8483276e6cb
dukudhay <90886985+udhayakumar-18@users.noreply.github.com>
2024-09-14 12:28:07 +0530

    commit udhay (14-Sep-2024) Added multi select in NA

--- SMS/sms_app/sub_forms/pk_needassessmentForm_form.py
+++ SMS/sms_app/sub_forms/pk_needassessmentForm_form.py
@@ -1,29 +1,36 @@
 from django import forms
+from django_select2.forms import Select2MultipleWidget
 from ..models import PkneedassessmentInfo
+from ..sub_models.na_typeofaccess_mod import Natypeofaccess
+
 
 class PkneedassessmentForm(forms.ModelForm):
+    # Many-to-Many field for multiple selections
+    na_type_of_access = forms.ModelMultipleChoiceField(
+        queryset=Natypeofaccess.objects.all(),
+        widget=Select2MultipleWidget(),  # Optional: provides enhanced multi-select UI
+        required=False
+    )
 
     class Meta:
         model = PkneedassessmentInfo
         fields = '__all__'
-        widgets = {
-            'na_type_of_access': forms.SelectMultiple(),
-        }
 
     def __init__(self, *args, **kwargs):
-        super(PkneedassessmentForm,self).__init__(*args, **kwargs)
+        super(PkneedassessmentForm, self).__init__(*args, **kwargs)
         self.fields['na_customer_name'].empty_label = "--Select--"
         self.fields['na_type_of_work'].empty_label = "--Select--"
         self.fields['na_type_of_pack'].empty_label = "--Select--"
         self.fields['na_wood_treatment_req'].empty_label = "--Select--"
-        self.fields['na_special_requirements'].empty_label = "--Select--"
+        # self.fields['na_special_requirements'].empty_label = "--Select--"
         self.fields['na_unloading'].empty_label = "--Select--"
-        self.fields['na_wood_norms'].empty_label = "--Select--"
+        # self.fields['na_wood_norms'].empty_label = "--Select--"
         self.fields['na_delivery_by'].empty_label = "--Select--"
         self.fields['na_delivery_type'].empty_label = "--Select--"
         self.fields['na_vehicle_type'].empty_label = "--Select--"
-        self.fields['na_type_of_access'].empty_label = "--Select--"
-        self.fields['na_consumables'].empty_label = "--Select--"
+        # Commented out since ManyToManyField does not need an empty label
+        # self.fields['na_type_of_access'].empty_label = "--Select--"
+        # self.fields['na_consumables'].empty_label = "--Select--"
         self.fields['na_packing_field'].empty_label = "--Select--"
         self.fields['na_updated_by'].empty_label = "--Select--"
         self.fields['na_status'].empty_label = "--Select--"
--- SMS/sms_app/sub_models/pk_needassessment_mod.py
+++ SMS/sms_app/sub_models/pk_needassessment_mod.py
@@ -1,38 +1,40 @@
 from django.db import models
+from ..models import StatusList, MyUser, Natypeofwork, Nadeliverytype, Naspecialrequirements, Napackingfield, Natypeofpack, CustomerInfo, Nawoodtreatmentreq, Nabvmcustomer, Nawoodnorms, VehicletypeInfo, Natypeofaccess, Stockdescription
 
-from ..models import StatusList,MyUser,Natypeofwork,Nadeliverytype,Naspecialrequirements,Napackingfield,Natypeofpack,CustomerInfo,Nawoodtreatmentreq,Nabvmcustomer,Nawoodnorms,VehicletypeInfo,Natypeofaccess,Stockdescription
-
+# Function to handle file upload path
 def Pkneedassessment_directory_path(instance, filename):
     return 'Pkneedassessmentfiles/{0}/{1}'.format(instance.na_assessment_num, filename)
+
+
 class PkneedassessmentInfo(models.Model):
-    na_assessment_num = models.CharField(max_length=100,null=True,blank=True, default='')
+    na_assessment_num = models.CharField(max_length=100, null=True, blank=True, default='')
     na_date = models.DateField(blank=True, null=True)
     na_customer_name = models.ForeignKey(CustomerInfo, on_delete=models.CASCADE, default='')
-    na_type_of_work = models.ForeignKey(Natypeofwork,on_delete=models.CASCADE,default='')
-    na_type_of_pack= models.ForeignKey(Natypeofpack,on_delete=models.CASCADE,default='')
+    na_type_of_work = models.ForeignKey(Natypeofwork, on_delete=models.CASCADE, default='')
+    na_type_of_pack = models.ForeignKey(Natypeofpack, on_delete=models.CASCADE, default='')
     na_wood_treatment_req = models.ForeignKey(Nawoodtreatmentreq, on_delete=models.CASCADE, default='')
-    na_unloading = models.ForeignKey(Nabvmcustomer,on_delete=models.CASCADE, related_name='na_unloading',db_column='na_unloading',default='')
-    na_wood_norms =models.ForeignKey(Nawoodnorms,on_delete=models.CASCADE,default='')
-    na_delivery_by = models.ForeignKey(Nabvmcustomer,on_delete=models.CASCADE, related_name='na_delivery_by',db_column='na_delivery_by',null=True,blank=True,default='')
-    na_vehicle_type = models.ForeignKey(VehicletypeInfo,on_delete=models.CASCADE,default='')
-    # na_type_of_access = models.ForeignKey(Natypeofaccess,on_delete=models.CASCADE,default='')
+    na_unloading = models.ForeignKey(Nabvmcustomer, on_delete=models.CASCADE, related_name='na_unloading', db_column='na_unloading', default='')
+    na_wood_norms = models.ForeignKey(Nawoodnorms, on_delete=models.CASCADE, default='')
+    na_delivery_by = models.ForeignKey(Nabvmcustomer, on_delete=models.CASCADE, related_name='na_delivery_by', db_column='na_delivery_by', null=True, blank=True, default='')
+    na_vehicle_type = models.ForeignKey(VehicletypeInfo, on_delete=models.CASCADE, default='')
+
+    # Updated field to ManyToMany for multi-select functionality
     na_type_of_access = models.ManyToManyField(Natypeofaccess)
-    na_consumables = models.ForeignKey(Stockdescription,on_delete=models.CASCADE,default='')
+
+    na_consumables = models.ForeignKey(Stockdescription, on_delete=models.CASCADE, default='')
     na_created_at = models.DateTimeField(null=True, auto_now_add=True)
     na_updated_at = models.DateTimeField(null=True, auto_now=True)
-    na_updated_by = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='na_updated_by',db_column='na_updated_by', null=True)
-    na_status = models.ForeignKey(StatusList, on_delete=models.CASCADE, default=6,blank=True,null=True)
-    na_attach = models.FileField(upload_to=Pkneedassessment_directory_path, null=True,blank=True)
-    na_others_type_pack = models.CharField(max_length=100,null=True,blank=True, default='')
-    na_delivery_type = models.ForeignKey(Nadeliverytype,on_delete=models.CASCADE,blank=True,null=True,default='')
-    na_packing_field = models.ForeignKey(Napackingfield,on_delete=models.CASCADE,blank=True,null=True,default='')
-    na_special_requirements = models.ForeignKey(Naspecialrequirements,on_delete=models.CASCADE,blank=True,null=True,default='')
-    na_delivery_location = models.CharField(max_length=100,null=True,blank=True, default='')
-
+    na_updated_by = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='na_updated_by', db_column='na_updated_by', null=True)
+    na_status = models.ForeignKey(StatusList, on_delete=models.CASCADE, default=6, blank=True, null=True)
+    na_attach = models.FileField(upload_to=Pkneedassessment_directory_path, null=True, blank=True)
+    na_others_type_pack = models.CharField(max_length=100, null=True, blank=True, default='')
+    na_delivery_type = models.ForeignKey(Nadeliverytype, on_delete=models.CASCADE, blank=True, null=True, default='')
+    na_packing_field = models.ForeignKey(Napackingfield, on_delete=models.CASCADE, blank=True, null=True, default='')
+    na_special_requirements = models.ForeignKey(Naspecialrequirements, on_delete=models.CASCADE, blank=True, null=True, default='')
+    na_delivery_location = models.CharField(max_length=100, null=True, blank=True, default='')
 
     class Meta:
         ordering = ["na_assessment_num"]
-
 
     def __str__(self):
         return self.na_assessment_num
