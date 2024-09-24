551ffc586850364495333b9b48c9b90b2aed595a
dukudhay <90886985+udhayakumar-18@users.noreply.github.com>
2024-09-24 14:27:34 +0530

    Added calculation in costing and quotation

--- SMS/sms_app/sub_models/pk_costing_mod.py
+++ SMS/sms_app/sub_models/pk_costing_mod.py
@@ -34,6 +34,8 @@ class PkcostingInfo(models.Model):
                                          db_column='ct_customer_name', blank=True, null=True)
     ct_customer_po = models.ForeignKey(PkpurchaseorderInfo, on_delete=models.CASCADE, related_name='ct_customer_po',
                                        db_column='ct_customer_po', blank=True, null=True)
+    ct_quantity_req= models.FloatField(blank=True, null=True, default=0.0)
+    ct_sqrt_req = models.FloatField(blank=True, null=True, default=0.0)
     class Meta:
         ordering = ["ct_cost_type"]
 
