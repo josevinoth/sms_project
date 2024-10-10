cd7808236373e84e618ec5a7a1039c4a7575f947
Hariharasudhan31012002 <hariharsudhanh968@gmail.com>
2024-10-10 18:44:43 +0530

    commit-Hari(10-Oct-2024) Image capture

--- SMS/sms_app/sub_models/camera_mod.py
+++ SMS/sms_app/sub_models/camera_mod.py
@@ -1,8 +1,10 @@
 from django.db import models
-
+from ..models import DamagereportInfo
 class CameraImage(models.Model):
     image = models.ImageField(upload_to='camera_images/')
     timestamp = models.DateTimeField(auto_now_add=True)
+    reference = models.ForeignKey(DamagereportInfo,null=True,on_delete=models.CASCADE)
+
 
     def __str__(self):
         return f"Image taken at {self.timestamp}"
