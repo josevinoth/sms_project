1a41af90c96e1af333a7a0354ccd1b2db889df63
Hariharasudhan31012002 <hariharsudhanh968@gmail.com>
2024-10-24 23:50:23 +0530

    commit-Hari(24-Oct-2024) Image capture

--- SMS/sms_app/sub_models/picture_mod.py
+++ SMS/sms_app/sub_models/picture_mod.py
@@ -1,9 +1,17 @@
 from django.db import models
 from ..models import damage_image_type_info,DamagereportInfo
+
+
+def picture_upload_path(instance, filename):
+    ext = filename.split('.')[-1]
+    image_type_name = instance.pi_image_type
+    filename = f"{image_type_name}_{instance.pi_reference.id}.{ext}"
+    return f"DamagereportPictures/{instance.pi_reference}/{filename}"
+
 class PictureImage(models.Model):
     pi_reference = models.ForeignKey(DamagereportInfo,on_delete=models.CASCADE)
     pi_image_type = models.ForeignKey(damage_image_type_info,on_delete=models.CASCADE)
-    pi_image = models.ImageField(upload_to='picture/', blank=True, null=True)
+    pi_image = models.ImageField(upload_to=picture_upload_path, blank=True, null=True)
     pi_timestamp = models.DateTimeField(null=True, auto_now=True)
 
     def __str__(self):
