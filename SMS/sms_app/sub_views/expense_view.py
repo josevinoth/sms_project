834768862abefd15d6b57e5be8287bc379bb28b4
josevinoth <101917197+josevinoth@users.noreply.github.com>
2024-11-16 17:42:18 +0530

    commit-Jose (16-Nov-24) Expense report updated in WMS expense report

--- SMS/sms_app/sub_views/expense_view.py
+++ SMS/sms_app/sub_views/expense_view.py
@@ -77,7 +77,8 @@ def expense_list(request):
     organisation_id = request.session.get('ses_organisation_id')
     role_id = request.session.get('ses_role_id')
 
-    expense_list_val = ExpenseInfo.objects.filter(exp_business=organisation_id).order_by('-id')
+    # expense_list_val = ExpenseInfo.objects.filter(exp_business=organisation_id).order_by('-id')
+    expense_list_val = ExpenseInfo.objects.all().order_by('-id')
     paginator = Paginator(expense_list_val, 50)  # Adjust the pagination size if needed
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
