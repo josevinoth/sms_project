ba35a9354ac4698a8e0b7c1acbc396fbef968d4b
josevinoth <101917197+josevinoth@users.noreply.github.com>
2025-01-04 10:36:58 +0530

    commit-Jose (04-Jan-25) chart style

--- SMS/sms_app/static/common-chart-config.js
+++ SMS/sms_app/static/common-chart-config.js
@@ -1,65 +1,44 @@
-// common-chart-config.js
-const commonChartOptions = {
-    responsive: true,
-    plugins: {
-        legend: {
-            display: true,
-            position: 'top',
-            labels: {
-                font: {
-                    size: 14,
-                    weight: 'bold'
-                },
-                color: '#000'
-            }
-        },
-        datalabels: {
-            anchor: 'end',
-            align: 'top',
-            formatter: (value) => value,
-            font: {
-                size: 12,
-                weight: 'bold'
-            },
-            color: '#000',
-            backgroundColor: 'rgba(235, 233, 234, 0.8)',
-            borderRadius: 4,
-            padding: 5,
-            borderWidth: 1,
-            borderColor: '#000'
-        }
+// Common chart configurations
+const commonChartStyles = {
+    bar_chart_1: {
+        backgroundColor: 'rgba(54, 162, 235, 0.5)',
+        borderColor: 'rgb(54, 162, 235)',
+        borderWidth: 1,
+        barThickness: 50,
+        maxBarThickness: 50,
     },
-    scales: {
-        x: {
-            ticks: {
-                font: {
-                    size: 12,
-                    weight: 'bold'
-                },
-                color: '#000'
-            }
+    bar_chart_2: {
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        borderColor: 'rgb(255, 99, 132)',
+        borderWidth: 1,
+        barThickness: 50,
+        maxBarThickness: 50,
+    },
+    legendStyle: {
+        display: true,
+        position: 'top',
+        labels: {
+            font: { size: 14, weight: 'bold' },
+            color: '#000',
         },
-        y: {
-            beginAtZero: true,
-            title: {
-                display: true,
-                text: 'Number of Calls',
-                font: {
-                    size: 16,
-                    weight: 'bold'
-                },
-                color: '#000'
-            },
-            ticks: {
-                font: {
-                    size: 10,
-                    weight: 'bold'
-                },
-                color: '#000'
-            }
-        }
-    }
+    },
+    yAxisStyle: {
+        beginAtZero: true,
+        title: {
+            display: true,
+            text: 'Number of Calls',
+            font: { size: 16, weight: 'bold' },
+            color: '#000',
+        },
+        ticks: {
+            font: { size: 10, weight: 'bold' },
+            color: '#000',
+        },
+    },
+    xAxisStyle: {
+        ticks: {
+            font: { size: 12, weight: 'bold' },
+            color: '#000',
+        },
+    },
 };
-
-// Export this configuration for reuse
-export default commonChartOptions;
