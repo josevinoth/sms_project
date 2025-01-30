e458f8086d1abf6e02bb5a67c53175759417560a
poojauday <23pooji@mail.com>
2025-01-30 19:27:22 +0530

    Commit Finance Reports(30-01-2025) Commit by Poojitha

--- SMS/sms_app/views.py
+++ SMS/sms_app/views.py
@@ -137,4 +137,4 @@ from .sub_views.warehouse_goods_add_new_view import warehouse_goods_add_gatein,w
 from .sub_views.salesperson_chart_view import targets_actuals_table
 from .sub_views.finance_reports_view import branch_profit_loss
 from .sub_views.finance_reports_view import branch_unit_profit_loss
-from .sub_views.finance_reports_view import finance_reports,businessmodel_PL,customerwise_PL
+from .sub_views.finance_reports_view import finance_reports,businessmodel_PL,customerwise_PL,fin_profit_loss_view,expenses_report,ar_due_reports
