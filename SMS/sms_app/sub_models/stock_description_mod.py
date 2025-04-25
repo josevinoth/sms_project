7a0f7c9847edfde8dc8a53383e81f42ef89eb2b0
dukudhay <90886985+udhayakumar-18@users.noreply.github.com>
2025-04-25 11:38:14 +0530

    Udhay - 25-April-2024 commit changes

--- SMS/sms_app/sub_models/stock_description_mod.py
+++ SMS/sms_app/sub_models/stock_description_mod.py
@@ -1,10 +1,11 @@
 from django.db import models
-from ..models import Pkstocktype
+from ..models import Pkstocktype,Unitofmeasure
 
 class Stockdescription(models.Model):
     stock_description = models.CharField(max_length=100, default='')
     stock_type = models.ForeignKey(Pkstocktype,on_delete=models.CASCADE,default='')
-
+    stock_received = models.ForeignKey(Unitofmeasure, on_delete=models.CASCADE,related_name='stock_received', db_column='stock_received',null=True, blank=True)
+    stock_Consumption = models.ForeignKey(Unitofmeasure, on_delete=models.CASCADE,related_name='stock_Consumption', db_column='stock_Consumption',null=True, blank=True)
 
     class Meta:
         ordering = ["stock_description"]
