ceafbf1ec3236b9da4ff6d05591e3b0d7ffa71c0
poojitha <23pooji@gmail.com>
2025-10-15 17:08:50 +0530

    commit poojitha(15-10-2025)gatein_mod,gatein html,dsr view,warehouse email view

--- SMS/sms_app/sub_views/warehouse_email_view.py
+++ SMS/sms_app/sub_views/warehouse_email_view.py
@@ -4,6 +4,7 @@ from ..forms import warehouse_EmailForm
 from ..sub_models.gatein_mod import Gatein_info
 from ..views import send_department_email
 
+
 def warehouse_send_email_view(request):
     if request.method == 'POST':
         form_warehouse_email = warehouse_EmailForm(request.POST)
@@ -11,10 +12,16 @@ def warehouse_send_email_view(request):
             recipient = form_warehouse_email.cleaned_data['recipient']
             subject = form_warehouse_email.cleaned_data['subject']
             message = form_warehouse_email.cleaned_data['message']
-            job_number=request.session.get('ses_gatein_id_nam')
-            gatein_email_count=Gatein_info.objects.get(gatein_job_no=job_number).gatein_email_count
+
+            message = message.replace('\n', '<br>')
+
+            job_number = request.session.get('ses_gatein_id_nam')
+            gatein_email_count = Gatein_info.objects.get(gatein_job_no=job_number).gatein_email_count
+
             recipient_list = [email.strip() for email in recipient.split(',')]
-            send_department_email('warehouse', subject, message, recipient_list)
-            gatein_email_count=gatein_email_count+1
+            send_department_email('warehouse',subject,message,recipient_list,email_type=1)
+            gatein_email_count += 1
             Gatein_info.objects.filter(gatein_job_no=job_number).update(gatein_email_count=gatein_email_count)
             return redirect(request.META['HTTP_REFERER'])
+
+
