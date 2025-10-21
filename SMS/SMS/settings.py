0db2aefcf5a9e85201ec91e509f01458a79c2176
Admin <udhayakumar.d@r2techsolutions.in>
2025-10-21 14:42:58 +0530

    consingment view Commit Udhay[21/10/25]

--- SMS/SMS/settings.py
+++ SMS/SMS/settings.py
@@ -85,9 +85,9 @@ WSGI_APPLICATION = 'SMS.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sms_002',
+        'NAME': 'asset_mgt_012',
         'USER': 'postgres',
-        'PASSWORD': 'root',
+        'PASSWORD': '244613',
         'HOST': 'localhost'
     }
 }
