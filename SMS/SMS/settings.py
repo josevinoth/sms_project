af2abdc50fdab1a1ac6078449364cb2327755dbc
josevinoth <101917197+josevinoth@users.noreply.github.com>
2026-04-26 10:12:43 +0530

    commit-Jose (26-Apr-26) latest update

--- SMS/SMS/settings.py
+++ SMS/SMS/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'SMS.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'asset_mgt_019',
+        'NAME': 'asset_mgt_001',
         'USER': 'postgres',
         'PASSWORD': '244613',
         'HOST': 'localhost'
