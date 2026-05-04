7856ac782f54e1d5d5655970a2908553a05be731
Admin <udhayakumar.d@r2techsolutions.in>
2026-05-04 11:05:39 +0530

    PMS changes COMMIT UDHAY[04-05-2026]

--- SMS/SMS/settings.py
+++ SMS/SMS/settings.py
@@ -171,14 +171,14 @@ DEPARTMENT_EMAILS = {
         'EMAIL_PORT' : 587,
         'EMAIL_USE_TLS' : True,
         'EMAIL_HOST_USER' : 'wms@thebvmgroup.com',
-        'EMAIL_HOST_PASSWORD' : 'F!220749109956aq',
+        'EMAIL_HOST_PASSWORD' : 'Bvm!123456789',
     },
     'itadmin': {
         'EMAIL_HOST' : 'smtp.office365.com',
         'EMAIL_PORT' : 587,
         'EMAIL_USE_TLS' : True,
         'EMAIL_HOST_USER' : 'itadmin@thebvmgroup.com',
-        'EMAIL_HOST_PASSWORD' : 'N/403017869523up',
+        'EMAIL_HOST_PASSWORD' : 'Bvm!123456789',
     }
 }
 
