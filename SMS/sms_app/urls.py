fa0d726bcf0f149d421da71d49c797a3c5939df1
joshuvaGit <joshuvabrighton17@gmail.com>
2026-05-14 11:49:18 +0530

    Commit by Joshuva(14/5/26) tms dashboard created and pushed

--- SMS/sms_app/urls.py
+++ SMS/sms_app/urls.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 
 from . import views
 from .sub_views.pk_purchaseorder_view import pk_create_batch_job, pk_get_po_items_for_job
+from .sub_views.tms_dashboard_view import tms_dashboard, get_tms_dashboard_data
 
 urlpatterns = [
     path('pk_create_batch_job/', pk_create_batch_job, name='pk_create_batch_job'),
@@ -930,6 +931,8 @@ urlpatterns = [
     path('customer_claims_report/', views.customer_claims_report, name='customer_claims_report'),
     path('highvalue_report_list/', views.highvalue_report_list, name='highvalue_report_list'),
     path('warehouse_dashboard/', views.warehouse_dashboard, name='warehouse_dashboard'),
+    path('tms_dashboard/', tms_dashboard, name='tms_dashboard'),
+    path('get_tms_dashboard_data/', get_tms_dashboard_data, name='get_tms_dashboard_data'),
     path('customer_contract_rate_report/', views.customer_contract_rate_report, name='customer_contract_rate_report'),
     path("get-customer-pan-gst/", views.get_customer_pan_gst, name="get_customer_pan_gst"),
     path('vehicle_allotment_email/', views.vehicle_allotment_email, name='vehicle_allotment_email'),
