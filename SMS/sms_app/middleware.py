baf3979314fb9936208e7ee6dae00664deca8fe1
Deepan <deepanprabhakaran02@gmail.com>
2026-06-22 18:11:24 +0530

    commit by deepan (22/06/2026) attached bill total km

---
+++ SMS/sms_app/middleware.py
@@ -0,0 +1,9 @@
+from django.contrib import messages
+
+class ClearMessagesMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
