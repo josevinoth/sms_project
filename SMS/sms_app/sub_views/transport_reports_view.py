e2763f486df866dd6286037d7586bd675dde03bd
Deepan <deepanprabhakaran02@gmail.com>
2026-07-28 16:30:54 +0530

    commit by deepan (28/07/2026)

--- SMS/sms_app/sub_views/transport_reports_view.py
+++ SMS/sms_app/sub_views/transport_reports_view.py
@@ -1974,7 +1974,8 @@ def invoice_pending_report_ajax_view(request):
 
     # -------------------------
     trips = TripdetailInfo.objects.filter(
-        tr_category_id=1
+        tr_category_id=1,
+        tr_departeddate_pickup__date__gte='2026-06-01'
     ).filter(
         Q(tr_consignmentnumber__co_consignmentnumber__icontains='MAA') |
         Q(tr_consignmentnumber__co_consignmentnumber__icontains='BLR')
