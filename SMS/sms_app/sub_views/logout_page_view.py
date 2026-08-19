638f11a2f48e72d0c91ad26ca7c1e1df678e17bb
Admin <udhayakumar.d@r2techsolutions.in>
2026-08-19 12:40:10 +0530

    mbl apk , scroll ,tripclosure overwrite COMMIT UDHAY[19-08-2026]

--- SMS/sms_app/sub_views/logout_page_view.py
+++ SMS/sms_app/sub_views/logout_page_view.py
@@ -2,7 +2,8 @@ from django.contrib.auth import logout
 from django.shortcuts import redirect
 
 def logout_page(request):
-    business_id = request.GET.get('business_id') or request.session.get('ses_organisation_id')
+    # Only redirect to customer login if explicitly requested via GET param from Customer Portal
+    business_id = request.GET.get('business_id')
     logout(request)
     
     if business_id:
